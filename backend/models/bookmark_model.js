1e13beb36e6b47c9395e61ec3dcc765465b826d4
damien duport percier <damienviel69@gmail.com>
2024-05-29 12:17:41 -0400

    Last commit

--- backend/models/bookmark_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const bookmarkSchema = new mongoose.Schema({
-  comment: {type: String, required: true},
-  movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true},
-  listId: { type: mongoose.Schema.Types.ObjectId, ref: 'List', required: true}
-})
-
-module.exports = mongoose.model('Bookmark', bookmarkSchema);
